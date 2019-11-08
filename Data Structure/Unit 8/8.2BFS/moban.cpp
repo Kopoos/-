8e86f43340da73ae80407a213f4704de0d966761
wusier <53465871+wusier@users.noreply.github.com>
2019-11-08 14:16:40 +0800

    Add files via upload

---
+++ Data Structure/Unit 8/8.2BFS/moban.cpp
@@ -0,0 +1,12 @@
+void BFS(int s)
+{
+    queue<int>q;
+    q.push(s);
+    while(!q.empty())
+    {
+        //取出队首元素top;
+        //访问队首元素top;
+        //将队首元素出队;
+        //将top的下一层结点中未曾入队的结点全部入队，并设置为已入队;
+    }
+}
